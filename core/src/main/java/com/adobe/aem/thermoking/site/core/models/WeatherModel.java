ce5d32dc875ed7b9a36541d677d4e00c77fc0274
Kamlesh <kamlesh@digiquadsolutions.com>
2022-09-20 20:06:20 +0530

    final commit

--- core/src/main/java/com/adobe/aem/thermoking/site/core/models/WeatherModel.java
+++ core/src/main/java/com/adobe/aem/thermoking/site/core/models/WeatherModel.java
@@ -1,64 +1,29 @@
 package com.adobe.aem.thermoking.site.core.models;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import javax.inject.Inject;
+
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import com.adobe.aem.thermoking.site.core.services.IWeatherOSGiService;
 
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class WeatherModel {
-    @ValueMapValue
-    public static String location;
-        private String weatherReport() {
-        try {
-            String unitGroup = "metric";
-            String apiKey = "BNQ99TYELHKNMVGT6SMGC45CU";
-            String apiEndPoint = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
-            URIBuilder builder = new URIBuilder(apiEndPoint + URLEncoder
-                    .encode("Hyderabad", StandardCharsets
-                            .UTF_8.toString()));
-            builder.setParameter("unitGroup", unitGroup)
-                    .setParameter("key", apiKey);
-            HttpGet get = new HttpGet(builder.build());
 
-            CloseableHttpClient httpclient = HttpClients.createDefault();
+	@Inject
+	IWeatherOSGiService iWeatherOSGiService;
 
-            CloseableHttpResponse response = httpclient.execute(get);
+	@ValueMapValue
+	public static String location;
 
-            String rawResult = "";
+	public String getWeatherDetails() {
+		return iWeatherOSGiService.getWeatherReportDetails(location);
+	}
 
-            if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
-                System.out.printf("Bad response status code:%d%n",
-                        response.getStatusLine().getStatusCode());
-                return unitGroup;
-            }
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                rawResult = EntityUtils.toString(entity, Charset.forName("utf-8"));
-            }
-            return rawResult;
-        } catch (Exception e) {
-
-        }
-        return "";
-    }
-
-    public String getWeatherDetails() {
-
-        return weatherReport();
-    }
-
+	public static String getLocation() {
+		return location;
+	}
 
 }
--- core/src/main/java/com/adobe/aem/thermoking/site/core/services/WeatherOSGiServiceImpl.java
+++ core/src/main/java/com/adobe/aem/thermoking/site/core/services/WeatherOSGiServiceImpl.java
@@ -1,5 +1,11 @@
 package com.adobe.aem.thermoking.site.core.services;
 
+import static com.adobe.aem.thermoking.site.core.models.WeatherModel.location;
+
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -12,56 +18,47 @@ import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-
-import static com.adobe.aem.thermoking.site.core.models.WeatherModel.location;
-
 @Component(service = IWeatherOSGiService.class, immediate = true)
 public class WeatherOSGiServiceImpl implements IWeatherOSGiService {
-    private static final Logger logger = LoggerFactory.getLogger(WeatherOSGiServiceImpl.class);
+	private static final Logger logger = LoggerFactory.getLogger(WeatherOSGiServiceImpl.class);
 //    @ValueMapValue
 //    public static String location;
 
-    @Override
-    public String getWeatherReportDtails(String location) {
-        try {
-            String unitGroup = "metric";
-            String apiKey = "BNQ99TYELHKNMVGT6SMGC45CU";
-            String apiEndPoint = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
-            URIBuilder builder = new URIBuilder(apiEndPoint + URLEncoder
-                    .encode(location, StandardCharsets
-                            .UTF_8.toString()));
-            builder.setParameter("unitGroup", unitGroup)
-                    .setParameter("key", apiKey);
-            HttpGet get = new HttpGet(builder.build());
+	@Override
+	public String getWeatherReportDetails(String location) {
+		try {
+			String unitGroup = "metric";
+			String apiKey = "BNQ99TYELHKNMVGT6SMGC45CU";
+			String apiEndPoint = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+			URIBuilder builder = new URIBuilder(
+					apiEndPoint + URLEncoder.encode(location, StandardCharsets.UTF_8.toString()));
+			builder.setParameter("unitGroup", unitGroup).setParameter("key", apiKey);
+			HttpGet get = new HttpGet(builder.build());
 
-            CloseableHttpClient httpclient = HttpClients.createDefault();
+			CloseableHttpClient httpclient = HttpClients.createDefault();
 
-            CloseableHttpResponse response = httpclient.execute(get);
+			CloseableHttpResponse response = httpclient.execute(get);
 
-            String rawResult = "";
+			String rawResult = "";
 
-            if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
-                System.out.printf("Bad response status code:%d%n",
-                        response.getStatusLine().getStatusCode());
-                return unitGroup;
-            }
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                rawResult = EntityUtils.toString(entity, Charset.forName("utf-8"));
-            }
-            return rawResult;
-        } catch (Exception e) {
+			if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+				System.out.printf("Bad response status code:%d%n", response.getStatusLine().getStatusCode());
+				return unitGroup;
+			}
+			HttpEntity entity = response.getEntity();
+			if (entity != null) {
+				rawResult = EntityUtils.toString(entity, Charset.forName("utf-8"));
+			}
+			return rawResult;
+		} catch (Exception e) {
 
-        }
-        return "";
-    }
+		}
+		return "";
+	}
 
-    public String getWeatherDetails() {
+	public String getWeatherDetails() {
 
-        return getWeatherReportDtails(location);
-    }
+		return getWeatherReportDetails(location);
+	}
 
 }
